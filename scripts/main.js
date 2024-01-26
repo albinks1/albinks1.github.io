91e984cf9ec1d926b2781e592212c1a337f88d33
albinpl <143488147+albinks1@users.noreply.github.com>
2024-01-26 19:29:08 +0100

    mise en place de la page d'accueil 1

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,35 @@
-let myImage = document.querySelector("img");
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+          const delay = entry.target.getAttribute('data-delay');
+          entry.target.style.transitionDelay = `${delay}ms`;
+          entry.target.style.opacity = "1";
+      }
+  });
+}, { rootMargin: "-30px" });
 
-myImage.addEventListener("click", function () {
-  let mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/300sl.png") {
-    myImage.setAttribute("src", "images/275gtb.png");
-  } else {
-    myImage.setAttribute("src", "images/300sl.png");
-  }
+document.querySelectorAll('.bloc').forEach((bloc) => {
+  observer.observe(bloc);
 });
 
-let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1");
+
+
+
+
+
+// let myImage = document.querySelector("img");
+
+// myImage.addEventListener("click", function () {
+//   let mySrc = myImage.getAttribute("src");
+//   if (mySrc === "images/300sl.png") {
+//     myImage.setAttribute("src", "images/275gtb.png");
+//   } else {
+//     myImage.setAttribute("src", "images/300sl.png");
+//   }
+// });
+
+// let myButton = document.querySelector("button");
+// let myHeading = document.querySelector("h1");
 
 // function setUserName() {
 //     let myName = prompt("Veuillez saisir votre nom.");
@@ -27,14 +46,14 @@ let myHeading = document.querySelector("h1");
 //     setUserName();
 //   });
   
-window.onscroll = function() {scrollFunction()};
+// window.onscroll = function() {scrollFunction()};
 
-  function scrollFunction() {
-    var header = document.getElementById("myHeader");
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-      header.style.height = "50px";
-    } else {
-      header.style.height = "100px";
-    }
-  }
+//   function scrollFunction() {
+//     var header = document.getElementById("myHeader");
+//     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//       header.style.height = "50px";
+//     } else {
+//       header.style.height = "100px";
+//     }
+//   }
   
